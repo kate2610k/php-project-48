49566beab0f71042903d26634fb7f84bfe37655c
kate2610k <ekaterinakarpova2610@gmail.com>
2024-08-14 00:19:39 +1000

    Fix 1

--- src/Parser.php
+++ src/Parser.php
@@ -7,7 +7,7 @@ use Symfony\Component\Yaml\Yaml;
 function parse(string $file)
 {
     $extension = pathinfo($file, PATHINFO_EXTENSION);
-    $content = file_get_contents($file, true);
+    $content = (string) file_get_contents($file);
 
     if ($extension == 'yml' || $extension == 'yaml') {
         return Yaml::parse($content);
